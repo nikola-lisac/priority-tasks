c36156aa39aab4eb607d033abcedc4042e486b59
BojanJakic <bojanjakic1@gmail.com>
2017-08-16 13:12:36 +0200

    #5 Create TaskRepository interface

---
+++ src/main/java/ba/codecentric/prioritytasks/repository/TaskRepository.java
@@ -0,0 +1,8 @@
+package ba.codecentric.prioritytasks.repository;
+
+
+import ba.codecentric.prioritytasks.domain.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task,Integer> {
+}
