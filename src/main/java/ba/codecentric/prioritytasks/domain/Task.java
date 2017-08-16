bbf256659828bfbcae2d37bc18f7c71b3f84864e
BojanJakic <bojanjakic1@gmail.com>
2017-08-16 16:02:21 +0200

    #5 Use javax.persistence.Entity instead of org.hibernate.annotations.Entity

--- src/main/java/ba/codecentric/prioritytasks/domain/Task.java
+++ src/main/java/ba/codecentric/prioritytasks/domain/Task.java
@@ -1,7 +1,6 @@
 package ba.codecentric.prioritytasks.domain;
 
-import org.hibernate.annotations.Entity;
-
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Date;
