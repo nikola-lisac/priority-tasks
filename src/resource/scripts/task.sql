e1ab8699f23ac99cb21873e56faa1daa0b705e78
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-08-16 16:08:38 +0200

    #4 Create "task" table

--- src/resource/scripts/task.sql
+++ src/resource/scripts/task.sql
@@ -1,6 +1,3 @@
-DROP SCHEMA IF EXISTS `priority_tasks`;
-CREATE SCHEMA `priority_tasks` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
-
 CREATE TABLE `priority_tasks`.`task` (
   `id` INT(11) NOT NULL,
   `name` VARCHAR(200) NOT NULL,
