bd606199652fc933bf90676f0a814902e33ee75b
Nikola Lisičić <nikola-lisac@users.noreply.github.com>
2017-08-16 10:42:32 +0200

    Create project structure (#9)
    * Create project structure
    
    * Bulk changes
    
    * Add gitignore file and rules for it
    * Delete intellij and gradle folders included in the last commit
    * Delete test module
    * Fix a typo in build.gradle

---
+++ src/main/java/ba/codecentric/prioritytasks/PriorityTasksApplication.java
@@ -0,0 +1,12 @@
+package ba.codecentric.prioritytasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorityTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriorityTasksApplication.class, args);
+    }
+}
