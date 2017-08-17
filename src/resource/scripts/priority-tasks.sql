19148f7df8712f92a83b32397c0aba8d2a191852
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-08-17 20:07:50 +0200

    #4 Change the data type for "completed" column to tinyint

--- src/resource/scripts/priority-tasks.sql
+++ src/resource/scripts/priority-tasks.sql
@@ -5,6 +5,6 @@ CREATE TABLE `priority_tasks`.`task` (
   `id` INT(11) NOT NULL,
   `name` VARCHAR(200) NOT NULL,
   `created_at` DATE NOT NULL,
-  `completed` BIT(1) NOT NULL,
+  `completed` TINYINT(1) NOT NULL,
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
