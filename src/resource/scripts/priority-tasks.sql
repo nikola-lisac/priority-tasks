30485e3750b64e7f993dbae76dd1beb332e267a3
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-08-20 19:29:22 +0200

    #4 Add AUTO_INCREMENT and delete task.sql

--- src/resource/scripts/priority-tasks.sql
+++ src/resource/scripts/priority-tasks.sql
@@ -2,7 +2,7 @@ DROP SCHEMA IF EXISTS `priority_tasks`;
 CREATE SCHEMA `priority_tasks` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 
 CREATE TABLE `priority_tasks`.`task` (
-  `id` INT(11) NOT NULL,
+  `id` INT(11) AUTO_INCREMENT NOT NULL,
   `name` VARCHAR(200) NOT NULL,
   `created_at` DATE NOT NULL,
   `completed` TINYINT(1) NOT NULL,
