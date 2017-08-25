545e36cbb0b621481e4d76d4e13a6cc7346c8434
BojanJakic <bojanjakic1@gmail.com>
2017-08-25 17:21:06 +0200

    #15 Add completeTask(Integer id)

--- src/main/java/ba/codecentric/prioritytasks/controller/TaskController.java
+++ src/main/java/ba/codecentric/prioritytasks/controller/TaskController.java
@@ -5,6 +5,7 @@ import ba.codecentric.prioritytasks.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,10 @@ public class TaskController {
             throw new Exception("No more tasks");
         }
         return tasks;
+    }
+
+    @DeleteMapping(value = "/tasks/{id}")
+    public void completeTask(@PathVariable Integer id) {
+        taskService.deleteTask(id);
     }
 }
